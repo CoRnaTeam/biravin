33117af9b31e8849da8f49f7a0c8471f81ecbcf0
Ravin <ravinrakholiya559@gmail.com>
2020-07-10 13:06:13 +0530

    mac project

---
+++ mac/shop/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'shop/index.html')
+
+
+def about(request):
+    return render(request,'shop/index.html')
+
+
+def contact(request):
+    return render(request,'shop/index.html')
+
+
+def tracker(request):
+    return render(request,'shop/index.html')
+
+
+def search(request):
+    return render(request,'shop/index.html')
+
+
+def prodview(request):
+    return render(request,'shop/index.html')
+
+
+def checkout(request):
+    return render(request,'shop/index.html')
+
+
+
